e8a13c9d258087707c40f72ecbd7e6aac87b1777
Santiago Botero <98826652+boterop@users.noreply.github.com>
2024-03-12 09:46:37 -0500

    Create redirection files (#2)
    * Remove old page when generates the same
    
    * Remove url to href with local files
    
    * replace web index to local index
    
    * Working completely
    
    * Put sub pages into subfolders with index.html
    
    * Improve code

--- bin/web.py
+++ bin/web.py
@@ -2,26 +2,60 @@ import requests
 import re
 from bin.storage import Storage
 
+LOCAL_INDEX_PATH = "/"
+
 
 class Web:
-    def __init__(self, url):
-        self.url = url
-        self.storage = Storage(self.get_domain(url))
+    def __init__(self, url: str):
+        self.index_url = url
+        domain = self.get_domain(url)
+        self.domain = domain
+        self.storage = Storage(domain)
 
-    def get_code(self):
-        return requests.get(self.url).text
+    def get_code(self, path: str = ""):
+        url = path if path != "" else self.index_url
+        return requests.get(url).text
 
-    def to_code(self):
-        self.code = requests.get(self.url).text
+    def to_code(self, path: str = ""):
+        url = path if path != "" else self.index_url
+        self.code = requests.get(url).text
         return self
 
-    def convert_to_local(self, code=None):
+    def convert_to_local(self, code: str = None, current_page: str = None):
         if code == None:
             code = self.code
 
-        self.storage.save("index.html", code)
+        if current_page == None:
+            current_page = self.index_url
 
-    def get_domain(self, url):
+        file_name = (
+            re.sub(self.index_url, "", current_page) + "/"
+            if current_page != self.index_url
+            else ""
+        ) + "index.html"
+
+        if self.storage.exist(file_name):
+            return
+
+        escaped_url = re.escape(current_page)
+        sub_links_regex = rf"\b({escaped_url}(?:\/[^\/\s]*)?)\b"
+        matches = re.findall(sub_links_regex, code)
+        matches = list(filter(lambda url: url != self.index_url, matches))
+
+        escaped_index = re.escape(self.index_url)
+        code_without_web_domain = re.sub(
+            rf"\b{escaped_index}\/?\b",
+            LOCAL_INDEX_PATH,
+            code,
+        )
+
+        self.storage.save(file_name, code_without_web_domain)
+
+        for sub_url in matches:
+            file_code = self.get_code(sub_url)
+            self.convert_to_local(file_code, sub_url)
+
+    def get_domain(self, url: str):
         domain_regex = re.compile(r"^(?:https?:\/\/)?(?:www\.)?([^\/]+)(?:\/.*)?$")
         match = domain_regex.match(url)
 
